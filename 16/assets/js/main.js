7ebd4205f24c5c0b010e35e47c0c35c2645dd2f2
Vitaliy <vitaliy.z125bpm@gmail.com>
2022-08-21 12:41:20 +0300

    upd

--- 16/assets/js/main.js
+++ 16/assets/js/main.js
@@ -76,7 +76,7 @@ function initSlickSlider() {
       {
         breakpoint: 480,
         settings: {
-          slidesToShow: 2,
+          slidesToShow: 1,
           slidesToScroll: 1
         }
       }
@@ -84,7 +84,40 @@ function initSlickSlider() {
 })};
 
 
+const offset = 100;
+const scrollUp = document.querySelector('.scroll-up');
+const scrollUpSvgPath = document.querySelector('.scroll-up__svg-path');
+const pathLength = scrollUpSvgPath.getTotalLength();
+
+scrollUpSvgPath.style.strokeDasharray = `${pathLength} ${pathLength}`;
+scrollUpSvgPath.style.transition = 'stroke-dashoffset 20ms';
 
 
+const getTop = () => window.pageYOffset || document.documentElement.scrollTop;
 
+// update dash offset
+const updateDashoffset = () => {
+  const height = document.documentElement.scrollHeight - window.innerHeight;
+  const dashoffset = pathLength - (getTop() * pathLength / height);
 
+  scrollUpSvgPath.style.strokeDashoffset = dashoffset;
+};
+
+// onscroll
+window.addEventListener('scroll', () => {
+  updateDashoffset();
+
+  if (getTop() > offset) {
+    scrollUp.classList.add('scroll-up--active');
+  } else {
+    scrollUp.classList.remove('scroll-up--active');
+  }
+});
+
+// click
+scrollUp.addEventListener('click', () => {
+  window.scrollTo({
+    top: 0,
+    behavior: 'smooth'
+  });
+});
