97054a258f94765208c27000e5d093435db97308
Vitaliy <vitaliy.z125bpm@gmail.com>
2022-08-27 19:41:14 +0300

    upd

--- 17/assets/js/main.js
+++ 17/assets/js/main.js
@@ -183,15 +183,3 @@ jarallax(document.querySelectorAll('.jarallax'), {
   
  
     
-  $.fn.parallax = function(resistance, mouse) {
-    $el = $(this);
-    TweenLite.to($el, 0.2, {
-      x: -((mouse.clientX - window.innerWidth / 2) / resistance),
-      y: -((mouse.clientY - window.innerHeight / 2) / resistance)
-    });
-  };
-  
-  $(document).mousemove(function(e) {
-   
-    $(".cloud3").parallax(30, e);
-    });   
